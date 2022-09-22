3d34d9aec8575f9e632cfd40fddeb44e087e92f7
Алиса Белич <alisacalm@yandex.ru>
2022-09-22 22:52:52 +0500

    Did homework

--- homework.py
+++ homework.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     packages: Sequence[Tuple[str, Union[int, float]]] = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),]
+        ('WLK', [9000, 1, 75, 180]), ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
